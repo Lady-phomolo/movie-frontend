bce995dc260f6e4afa24fd0cdb176954ab1ec3cc
phomolo <phomolo@megafreight.co.za>
2018-07-02 10:46:25 +0200

    modified user service

--- src/app/shared-service/user.service.ts
+++ src/app/shared-service/user.service.ts
@@ -15,27 +15,14 @@ export class UserService {
 
   constructor(private http:Http) { }
 
-  // register(user){
-  //   return this.http.post(this.baseUrl + "register",JSON.stringify(user) ,this.options)
-  //           .pipe(map((res:Response)=>res.json()));
-  // }
-
-  // login(user){
-  //   return this.http.get(this.baseUrl + 'login/' +user.email +'/'+ user.password,this.options)
-  //           .pipe(map((res:Response)=>res.json()));
-  // }
-
   register(user){
-    let headers = new Headers();
-    headers.append('Content-type','application/json');
-    return this.http.post('http://localhost:8080/api/register',user,{headers:headers})
+    return this.http.post('http://localhost:8080/api/register',user,this.options)
     .pipe(map(res=>res.text()));
   }
 
   login(user){
-    let headers = new Headers();
-    headers.append('Content-type','application/json');
-    return this.http.get('http://localhost:8080/api/login/'+ user.email +'/'+user.password,{headers:headers})
+  
+    return this.http.get('http://localhost:8080/api/login/'+ user.email +'/'+user.password,this.options)
      .pipe(map(res => res.text() ? res.json() : res));
   }
 }
